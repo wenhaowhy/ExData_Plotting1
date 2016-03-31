62e4e32ae8988f82401a15692bacecbdcfca9c2e
wenhaowhyy <wyu2013@gmail.com>
2016-03-30 22:08:59 -0400

    added all files

---
+++ Plots 1 to 4 (pc too slow to save individually).R
@@ -0,0 +1,42 @@
+setwd("C:/Users/sharon/Desktop/")
+
+plotData <- read.table(file.choose(), header=T, sep=";", na.strings="?")
+
+finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+finalData <- cbind(SetTime, finalData)
+
+## Generating Plot 1
+
+png(filename= "finalData",width=480,height=480,units='px')
+
+hist(finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+x<-dev.off()
+
+
+## Generating Plot 2
+plot(finalData$SetTime, finalData$Global_active_power, type="l", col="black", xlab="", ylab="Global Active Power (kilowatts)")
+
+
+###Plot 3
+columnlines <- c("black", "red", "blue")
+labels <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+plot(finalData$SetTime, finalData$Sub_metering_1, type="l", col=columnlines[1], xlab="", ylab="Energy sub metering")
+lines(finalData$SetTime, finalData$Sub_metering_2, col=columnlines[2])
+lines(finalData$SetTime, finalData$Sub_metering_3, col=columnlines[3])
+legend("topright", legend=labels, col=columnlines, lty="solid")
+
+
+##Plot4
+
+labels <- c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
+columnlines <- c("black","red","blue")
+par(mfrow=c(2,2))
+plot(finalData$SetTime, finalData$Global_active_power, type="l", col="green", xlab="", ylab="Global Active Power")
+plot(finalData$SetTime, finalData$Voltage, type="l", col="orange", xlab="datetime", ylab="Voltage")
+plot(finalData$SetTime, finalData$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
+lines(finalData$SetTime, finalData$Sub_metering_2, type="l", col="red")
+lines(finalData$SetTime, finalData$Sub_metering_3, type="l", col="blue")
+legend("topright", bty="n", legend=labels, lty=1, col=columnlines)
+plot(finalData$SetTime, finalData$Global_reactive_power, type="l", col="blue", xlab="datetime", ylab="Global_reactive_power")
